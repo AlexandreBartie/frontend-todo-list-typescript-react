d9bf0cf3d8b916983cd334a2db3f31654cf8e3f8
ALEXANDRE Bartie <alexandre.bartie-ext@hexagon.com>
2023-03-14 01:29:47 -0300

    feat: create profile component

--- src/elements/profile/profile.tsx
+++ src/elements/profile/profile.tsx
@@ -2,25 +2,34 @@ import React, { ReactElement } from "react"
 
 import { Avatar, Box, SxProps, Theme, Typography } from "@mui/material"
 
-
-
-
 export const Profile = (): ReactElement => {
-
-    return (
-    <Box display="flex" flexDirection="column">
-      <Avatar sx={avatarTheme()}>AB</Avatar>
-      <Typography>Boa Noite</Typography>
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Avatar sx={avatarTheme()}>
+        <Typography variant="h4" color="text.primary">
+          AB
+        </Typography>
+      </Avatar>
+      <Typography variant="h6" color="text.primary">
+        Welcome, Alexandre
+      </Typography>
+      <Typography variant="body1" color="text.primary">
+        This is your personal todo-list
+      </Typography>
     </Box>
   )
 }
 
-const avatarTheme = () : SxProps<Theme> => {
-    return {
-        width: '96px',
-        height: '96px',
-        backgroundColor: 'primary.main',
-        marginBottom: '16px'
-      }
+const avatarTheme = (): SxProps<Theme> => {
+  return {
+    width: "96px",
+    height: "96px",
+    backgroundColor: "primary.main",
+    marginBottom: "16px",
+  }
 }
-
